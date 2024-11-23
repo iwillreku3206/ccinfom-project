1ff587f3c9a0df7ccfa837653bbc2b6b1de2e93d
modavid <modavid.1964@gmail.com>
2024-11-23 12:58:05 +0800

    Create interface for admin game management.

--- controllers/model/gamesController.ts
+++ controllers/model/gamesController.ts
@@ -14,10 +14,10 @@ gameRouter.use(errorHandler(new Map([
 
 gameRouter.get('/', async (req: Request, res: Response) => {
   const { user, model, error } = res.locals;
-  const name = req?.body?.name;
+  const name = req?.query?.name?.toString().trim() ?? '';
 
   // Grab the games then render
-  const games = (!name || name.trim() == '')
+  const games = name == ''
    ? await model.getAllGames()
    : [ await model.getGame({ name }) ]
   const gameData = JSON.stringify(games)
@@ -33,6 +33,15 @@ gameRouter.get('/', async (req: Request, res: Response) => {
 gameRouter.post('/', async (req: Request, res: Response) => {
   const { user, model, error } = res.locals;
   const name = req?.body?.name;
+  res.redirect("/games?name=" + name)
+})
+
+gameRouter.get('/manage', async (req: Request, res: Response) => {
+  const { user, model, error } = res.locals;
+  const name = req?.body?.name;
+
+  if (user?.userType == 'basic')
+    return res.redirect('/games?error=adminsonly')
 
   // Grab the games then render
   const games = (!name || name.trim() == '')
@@ -40,7 +49,7 @@ gameRouter.post('/', async (req: Request, res: Response) => {
    : [ await model.getGame({ name }) ]
   const gameData = JSON.stringify(games)
 
-  render(res, "games", {
+  render(res, "manageGames", {
     username: user?.username, 
     displayName: user?.displayName || user?.username,
     gameData,
@@ -48,21 +57,9 @@ gameRouter.post('/', async (req: Request, res: Response) => {
   })
 })
 
-gameRouter.get('/manage', async (req: Request, res: Response) => {
-  const { user, model, error } = res.locals;
-
-  if (user?.userType == 'basic')
-    return res.redirect('/games?error=adminsonly')
-
-  render(res, "manageGames", {
-    username: user?.username, 
-    displayName: user?.displayName || user?.username,
-    error
-  })
-})
-
 gameRouter.post('/manage', async (req: Request, res: Response) => {
   const { user, model, error } = res.locals;
+  const name = req?.body?.name;
   
   try {
     await model.createGame({ name: req.body.name, description: req.body.description })
