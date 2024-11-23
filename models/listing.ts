0779d67be1c3f396b4bb82cc3593cf8bfc29da30
modavid <modavid.1964@gmail.com>
2024-11-23 19:48:25 +0800

    Create buy transaction.

--- models/listing.ts
+++ models/listing.ts
@@ -1,7 +1,7 @@
 /**
  * @ Author: Group ??
  * @ Create Time: 2024-11-16 10:43:56
- * @ Modified time: 2024-11-23 12:16:52
+ * @ Modified time: 2024-11-23 19:48:01
  * @ Description:
  * 
  * Manages mapping listings to runtime objects.
@@ -9,6 +9,7 @@
 
 import type { QueryResult, RowDataPacket } from "mysql2";
 import Model from "./model"
+import UserModel from "./user";
 
 // The interface
 export interface IListing {
@@ -22,12 +23,14 @@ export interface IListing {
 
 // Specs
 type create_listing_spec = Omit<IListing, 'id' | 'list_date' | 'sold'>
+type listing_buy_spec = { id: number, buyer_id: number }
 type get_listing_spec = Partial<IListing>
 type get_listing_by_game_spec = { game: number }
 
 // The model queries
 const create_listing_query = `INSERT INTO \`listings\` (item, price, seller) VALUES (?, ?, ?);`;
 const get_listings_query = `SELECT * FROM \`listings\`;`;
+const get_listing_by_id_query = `SELECT * FROM \`listings\` WHERE id = ?;`;
 const get_listing_by_item_query = `SELECT * FROM \`listings\` WHERE item = ?;`;
 const get_listing_by_price_query = `SELECT * FROM \`listings\` WHERE price BETWEEN ? AND ?;`;
 const get_listing_by_seller_query = `SELECT * FROM \`listings\` WHERE seller = ?;`;
@@ -37,6 +40,17 @@ const get_listing_by_game = `
     JOIN \`items\` i ON i.id = l.item 
     JOIN \`games\` g ON g.id = i.game 
     WHERE i.game = ?;`
+
+const create_sold_listing_query = `
+  INSERT INTO \`sold_listings\` 
+    (listing, buyer) VALUES (?, ?);
+`
+
+const update_sold_listing_by_id_query = `
+  UPDATE \`listings\`
+    SET sold = 1
+    WHERE id = ?;
+`
 
 const delete_listings_by_userid = `
   DELETE FROM \`listings\`
@@ -61,11 +75,14 @@ export default class ListingModel extends Model {
     super()
     super.register('create', create_listing_query, listing => [listing.item, listing.price, listing.seller])
     super.register('get-all', get_listings_query, _ => [])
+    super.register('get-by-id', get_listing_by_id_query, listing => [listing.id])
     super.register('get-by-item', get_listing_by_item_query, listing => [listing.item])
     super.register('get-by-price', get_listing_by_price_query, listing => [listing.min, listing.max])
     super.register('get-by-seller', get_listing_by_seller_query, listing => [listing.seller])
     super.register('get-by-list-date', get_listing_by_list_date_query, listing => [listing.min, listing.max])
     super.register('get-by-game', get_listing_by_game, listing => [listing.game])
+    super.register('create-sold', create_sold_listing_query, sold => [sold.listing, sold.buyer])
+    super.register('update-sold', update_sold_listing_by_id_query, sold => [sold.id])
     super.register('delete-listings-by-userid', delete_listings_by_userid, l => [l.userId])
   }
 
@@ -110,6 +127,27 @@ export default class ListingModel extends Model {
   async getGameListings(listing: get_listing_by_game_spec): Promise<IListing[] | null> {
     const results = await this.execute<RowDataPacket[]>('get-by-game', listing)
     return results.map((r: RowDataPacket) => (r as IListing))
+  }
+
+  async getListing(listing: get_listing_spec): Promise<IListing | null> {
+    const listings = await this.execute<RowDataPacket[]>('get-by-id', listing);
+    return listings.length ? listings[0] as IListing : null;
+  }
+
+  async buyListing(listing: listing_buy_spec) {
+    
+    // Get sold item
+    const sold = await this.getListing({ id: listing.id })
+    if(!sold) return;
+
+    // Create sold entry
+    await this.execute('create-sold', { listing: sold?.id, buyer: listing.buyer_id  })  
+    
+    // Mark as sold
+    await this.execute('update-sold', { id: listing.id })
+
+    // Subtracts the price of the item from the balance of the user
+    await UserModel.instance.updateBalance({ id: listing.buyer_id, balance: sold.price })
   }
 
   /**
