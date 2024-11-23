e90df36390960c912aae795c4cfc73a05f990f10
iwillreku3206 <37181282+iwillreku3206@users.noreply.github.com>
2024-11-23 18:13:55 +0800

    WIPwork on reports

---
+++ controllers/admin/reportsController.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import { isAdmin, isLoggedIn } from '../../util/plugins'
+import { render } from '../../util/io'
+
+export const reportsRouter = express.Router()
+
+reportsRouter.use(isLoggedIn)
+reportsRouter.use(isAdmin)
+
+reportsRouter.get('/', async (req, res) => {
+  const { user } = res.locals;
+  render(res, "reports", {
+    username: user?.username,
+    displayName: user?.displayName || user?.username
+  })
+})
